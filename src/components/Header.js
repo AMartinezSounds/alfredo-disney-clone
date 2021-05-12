36807d5f99055801f6f659f6116083092df4916e
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-12 18:43:49 +0200

    I've created an image carousel with the npm slick component

--- src/components/Header.js
+++ src/components/Header.js
@@ -57,6 +57,7 @@ const NavMenu = styled.div`
     flex: 1;
     margin-left: 25px;
     align-items: center;
+    overflow-x: hidden;
     a{
         display: flex;
         align-items: center;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import ImgSlider from './ImgSlider';
+
+
 
 function Home() {
     return (
         <Container>
-            Home
+            <ImgSlider/>
         </Container>
     )
-}
-
-export default Home
+};
 
 const Container = styled.main`
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
     position: relative;
+    overflow-x: hidden;
     &:before {
         background: url("/images/home-background.png") center center/cover no-repeat fixed;
         content: "";
@@ -23,5 +25,8 @@ const Container = styled.main`
         left: 0;
         right: 0;
         bottom: 0;
+        z-index: -1;
     }
 `;
+
+export default Home;
