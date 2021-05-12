274d55ffb3716cd521a577fb4bd905bc51b9716b
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-12 16:25:28 +0200

    clean starter for the disney project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
