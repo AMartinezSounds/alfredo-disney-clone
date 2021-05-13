313379f7c595bf8c9c5b55d504bd7158c048cce9
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-13 08:56:09 +0200

    I've created the detail page, added a background image and used React Router to change between Home and Detail

---
+++ src/components/Detail.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Detail() {
+    return (
+        <Container>
+            <Background>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" alt="" />
+            </Background>
+        </Container>
+    )
+};
+
+const Container = styled.div`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw  + 5px);
+    position: relative;
+`;
+
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    opacity: 0.8;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+export default Detail
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
+import Movies from './Movies';
 
 
 
@@ -10,6 +11,7 @@ function Home() {
         <Container>
             <ImgSlider/>
             <Viewers/>
+            <Movies/>
         </Container>
     )
 };
